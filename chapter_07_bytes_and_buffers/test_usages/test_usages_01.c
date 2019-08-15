4ade847b153e0cbe852ab2ea327f2cafaa456456
Mikail-Eliyah <Mikail.Eliyah@gmail.com>
2019-08-14 21:40:30 -0700

    Add test case to chapter_07

--- chapter_07_bytes_and_buffers/test_usages/test_usages_01.c
+++ chapter_07_bytes_and_buffers/test_usages/test_usages_01.c
@@ -2,7 +2,7 @@
 
 #define BUF_SIZE 16
 	
-extern struct item; // declare
+// extern struct item; // declare
 
 struct item { 
 	int* decValue[BUF_SIZE];
@@ -11,8 +11,10 @@ struct item {
 // struct item first; // instantiate
 struct item first = {
 	.decValue = {
-	0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 
-	0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a
+	//0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 
+	//0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a
+	(int) 0x6b, (int) 0xc1, (int) 0xbe, (int) 0xe2, (int) 0x2e, (int) 0x40, (int) 0x9f, (int) 0x96, 
+	(int) 0xe9, (int) 0x3d, (int) 0x7e, (int) 0x11, (int) 0x73, (int) 0x93, (int) 0x17, (int) 0x2a
 	}
 };
 
@@ -28,19 +30,28 @@ void hexStringToHexBytesArray_from_static_data (char* hexStringBytes) {
      char* hexByteInStr = "";
      //char byteInStr[3] = ""; // XX\0
      char byteInStr[5] = "0x"; // 0xXX\0 
-     int numberOfBytes = (strlen(hexStringBytes)/2);
+     int number_of_bytes = (strlen(hexStringBytes)/2);
      int i = 0;
+	 
+	 printf("number_of_bytes: %d \n", number_of_bytes);
                 
-        for(i = 0; i < numberOfBytes; i++){
-              strncat(byteInStr, &hexStringBytes[(i*2)], 1);       // insert the 1st hex character
-              strncat(byteInStr, &hexStringBytes[(i*2)+1], 1);  
-// insert the 2nd hex character
-              byteInStr[5] = '\0';
-             // decValue[i] = (int)strtol(byteInStr, NULL, 0);   // without "0x" header, last paramater is 16
-              first.decValue[i] = (int)strtol(byteInStr, NULL, 0);   // without "0x" header, last paramater is 16. Results store in array within struct
+    for(i = 0; i < number_of_bytes; i++){
+		printf("i of number_of_bytes: %d of %d \n", i, number_of_bytes);
+		
+		strncat(byteInStr, &hexStringBytes[(i*2)], 1);       // insert the 1st hex character
+		strncat(byteInStr, &hexStringBytes[(i*2)+1], 1);  
+		// insert the 2nd hex character
+		byteInStr[5] = '\0';
+			  
+		printf("byteInStr: %s \n", byteInStr);
+		// decValue[i] = (int)strtol(byteInStr, NULL, 0);   // without "0x" header, last paramater is 16
+		first.decValue[i] = (int)strtol(byteInStr, NULL, 0);   // without "0x" header, last paramater is 16. Results store in array within struct
               
-              strcpy(byteInStr, "0x");
-         }                 
+		strcpy(byteInStr, "0x");
+		printf("byteInStr: %s \n", byteInStr);
+		printf("i of number_of_bytes: %d of %d \n", i, number_of_bytes);
+		number_of_bytes = (strlen(hexStringBytes)/2);
+    }                 
 }
 
 
@@ -59,7 +70,8 @@ void hexStringToHexBytesArray (struct item* obj, char* hexStringBytes) {
 	 
      int i = 0;
                 
-    for(i = 0; i < numberOfBytes; i++){
+    // for(i = 0; i < numberOfBytes; i++){
+	for(i = 0; i < (strlen(hexStringBytes)/2); i++){	
 		strncat(byteInStr, &hexStringBytes[(i*2)], 1);       // insert the 1st hex character
         strncat(byteInStr, &hexStringBytes[(i*2)+1], 1);  
 // insert the 2nd hex character
@@ -68,7 +80,7 @@ void hexStringToHexBytesArray (struct item* obj, char* hexStringBytes) {
         obj->decValue[i] = (int)strtol(byteInStr, NULL, 0);   // without "0x" header, last paramater is 16. Results store in array within struct
               
          strcpy(byteInStr, "0x");
-        }                 
+    }                 
 }
 
 /*
@@ -104,8 +116,8 @@ int test_usage_hex_strings()
 	INFO(">> ");
 	
 	int* decValue[BUF_SIZE] = {
-	0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 
-	0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a
+	(int) 0x6b, (int) 0xc1, (int) 0xbe, (int) 0xe2, (int) 0x2e, (int) 0x40, (int) 0x9f, (int) 0x96, 
+	(int) 0xe9, (int) 0x3d, (int) 0x7e, (int) 0x11, (int) 0x73, (int) 0x93, (int) 0x17, (int) 0x2a
 	};
 	
 	int numberOfBytes = BUF_SIZE;
